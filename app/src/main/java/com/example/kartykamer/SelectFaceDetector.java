d379b5acb95a699e4e5473d7d205a26bf0e07f21
Maciej Adamski <madamskip1@gmail.com>
2021-09-07 12:55:31 +0200

    Select Face Detector layout with first algorithm

--- app/src/main/java/com/example/kartykamer/SelectFaceDetector.java
+++ app/src/main/java/com/example/kartykamer/SelectFaceDetector.java
@@ -3,11 +3,16 @@ package com.example.kartykamer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Camera;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.kartykamer.interfaces.FPSView;
 
 
-public class SelectFaceDetector extends AppCompatActivity {
+public class SelectFaceDetector extends AppCompatActivity implements FPSView {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,21 @@ public class SelectFaceDetector extends AppCompatActivity {
     }
 
     public void goHaarCascade(View view) {
+        setContentView(R.layout.camera_image_view);
+        ImageView imgView;
+        imgView = (ImageView) findViewById(R.id.camera_preview);
+        CameraCore cameraCore = new CameraCore(this);
+        cameraCore.setImageView(imgView);
+        cameraCore.setFpsView(this);
+        cameraCore.start();
+    }
+
+    public void setFPSText(double curFPS, double avgFPS) {
+        TextView curFPSTextView = (TextView) findViewById(R.id.cur_fps_text);
+        TextView avgFPSTextView = (TextView) findViewById(R.id.avg_fps_text);
+
+        curFPSTextView.setText(String.format("%.2f", curFPS));
+        avgFPSTextView.setText(String.format("%.2f", avgFPS));
 
     }
 }
