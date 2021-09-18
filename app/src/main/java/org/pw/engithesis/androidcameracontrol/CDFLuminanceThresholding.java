c4dd7698948cfd9f1919f45ac727bc4c013f6fe0
Maciej Adamski <madamskip1@gmail.com>
2021-09-18 13:37:04 +0200

    Cumulative Distribution thresholding

---
+++ app/src/main/java/org/pw/engithesis/androidcameracontrol/CDFLuminanceThresholding.java
@@ -0,0 +1,77 @@
+package org.pw.engithesis.androidcameracontrol;
+
+import org.pw.engithesis.androidcameracontrol.interfaces.ImageThresholding;
+
+import java.util.Arrays;
+
+public class CDFLuminanceThresholding extends ImageThresholding {
+    private static final double DEFAULT_THRESHOLD_PARAMETER = 0.05;
+
+    private double[] cumulativeDistributionOfLuminance;
+    private double[] counterPixelsLuminance;
+    private final double thresholdParameter;
+
+    public CDFLuminanceThresholding() {
+        thresholdParameter = DEFAULT_THRESHOLD_PARAMETER;
+        initArrays();
+    }
+
+    public CDFLuminanceThresholding(double threshold) {
+        thresholdParameter = threshold;
+        initArrays();
+    }
+
+
+    private void initArrays() {
+        cumulativeDistributionOfLuminance = new double[256];
+        Arrays.fill(cumulativeDistributionOfLuminance, 1.0);
+        counterPixelsLuminance = new double[256];
+    }
+
+    @Override
+    protected byte filterFunction(byte pixel) {
+        if (getCDF(pixel) <= thresholdParameter) {
+            return intToByte(255);
+        }
+
+        return intToByte(0);
+    }
+
+    private double getCDF(byte pixel) {
+        int luminance = byteToInt(pixel);
+        return cumulativeDistributionOfLuminance[luminance];
+    }
+
+    @Override
+    protected void beforeThresholding() {
+        countPixelsLuminance();
+        calcCDF();
+    }
+
+    private void countPixelsLuminance() {
+        int numPixels = (int) matToThreshold.total();
+        byte[] pixels = new byte[numPixels];
+        matToThreshold.get(0, 0, pixels);
+
+        for (int i = 0; i < numPixels; i++) {
+            int luminance = byteToInt(pixels[i]);
+            counterPixelsLuminance[luminance]++;
+        }
+    }
+
+    private void calcCDF() {
+        int numPixels = (int) matToThreshold.total();
+        cumulativeDistributionOfLuminance[0] = counterPixelsLuminance[0] / numPixels;
+
+        for (int i = 1; i < 256; i++) {
+            double probability = cumulativeDistributionOfLuminance[i - 1];
+            probability += counterPixelsLuminance[i] / numPixels;
+
+            cumulativeDistributionOfLuminance[i] = probability;
+
+            if (probability > thresholdParameter) {
+                i = 256;
+            }
+        }
+    }
+}
