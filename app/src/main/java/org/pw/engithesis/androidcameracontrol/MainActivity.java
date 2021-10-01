67370f8dc15ec867d325d504d7eaf57e7d55d0d1
Maciej Adamski <madamskip1@gmail.com>
2021-10-01 21:21:09 +0200

    Splash screen with WEITI Logo

--- app/src/main/java/org/pw/engithesis/androidcameracontrol/MainActivity.java
+++ app/src/main/java/org/pw/engithesis/androidcameracontrol/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.Theme_AndroidCameraControl);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
