8346af14852c174c174de9fd02a7e8a31d88dd03
Maciej Adamski <madamskip1@gmail.com>
2021-10-29 18:31:23 +0200

    Refactor Dlib face detectors

--- app/src/main/java/org/pw/engithesis/androidcameracontrol/facedetectionalgorithms/FaceDetectionDlibHOG.java
+++ app/src/main/java/org/pw/engithesis/androidcameracontrol/facedetectionalgorithms/FaceDetectionDlibHOG.java
@@ -3,9 +3,6 @@ package org.pw.engithesis.androidcameracontrol.facedetectionalgorithms;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-import org.pw.engithesis.androidcameracontrol.Utility;
 
 public class FaceDetectionDlibHOG implements FaceDetectionAlgorithm {
 
@@ -25,15 +22,8 @@ public class FaceDetectionDlibHOG implements FaceDetectionAlgorithm {
 
     @Override
     public Rect[] detect(Mat frame) {
-        Mat dupa = new Mat();
-        Imgproc.cvtColor(frame, dupa, Imgproc.COLOR_RGB2GRAY);
-
         MatOfRect faces = new MatOfRect();
         nativeDetect(nativeDetectorAddr, frame.getNativeObjAddr(), faces.getNativeObjAddr());
-
-
-        Utility.drawRects(frame, faces.toArray(), new Scalar(75, 75, 75));
         return faces.toArray();
     }
-
 }
--- app/src/main/java/org/pw/engithesis/androidcameracontrol/facedetectionalgorithms/FaceDetectionDlibMMOD.java
+++ app/src/main/java/org/pw/engithesis/androidcameracontrol/facedetectionalgorithms/FaceDetectionDlibMMOD.java
@@ -3,13 +3,15 @@ package org.pw.engithesis.androidcameracontrol.facedetectionalgorithms;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
 import org.pw.engithesis.androidcameracontrol.R;
 import org.pw.engithesis.androidcameracontrol.RawResourceManager;
-import org.pw.engithesis.androidcameracontrol.Utility;
 
 public class FaceDetectionDlibMMOD implements FaceDetectionAlgorithm {
     private final long nativeDetectorAddr;
+
+    static {
+        System.loadLibrary("dlib_natives");
+    }
 
     public FaceDetectionDlibMMOD() {
         RawResourceManager model = new RawResourceManager(R.raw.mmod_human_face_detector, "mmod_human_face_detector.dat");
@@ -24,8 +26,6 @@ public class FaceDetectionDlibMMOD implements FaceDetectionAlgorithm {
     public Rect[] detect(Mat frame) {
         MatOfRect faces = new MatOfRect();
         nativeDetect(nativeDetectorAddr, frame.getNativeObjAddr(), faces.getNativeObjAddr());
-
-        Utility.drawRects(frame, faces.toArray(), new Scalar(75, 75, 75));
         return faces.toArray();
     }
 }
