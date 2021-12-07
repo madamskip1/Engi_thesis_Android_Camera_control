e0915d61cad5f469773a056cc17a84a6cec0ab43
Maciej Adamski <madamskip1@gmail.com>
2021-12-07 12:36:24 +0100

    ImageSlider now use live camera frame to swap images + being observer

--- app/src/main/java/org/pw/engithesis/androidcameracontrol/ImageSlider.java
+++ app/src/main/java/org/pw/engithesis/androidcameracontrol/ImageSlider.java
@@ -5,21 +5,47 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
-public class ImageSlider extends AppCompatActivity {
+import org.pw.engithesis.androidcameracontrol.interfaces.Observer;
+
+public class ImageSlider extends AppCompatActivity implements Observer {
 
     int[] images = {R.drawable.portrait_test_1, R.drawable.author_name_text, R.drawable.portrait_test_2, R.drawable.lenna, R.drawable.portrait_test_4};
     ImageSliderPagerAdapter imageSliderPagerAdapter;
     ViewPager viewPager;
+    CameraControlMainClass cameraControlMainClass;
+    EyeMoveDetector eyeMoveDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_slider);
 
+        cameraControlMainClass = new CameraControlMainClass(this);
         viewPager = (ViewPager) findViewById(R.id.image_slider);
 
         imageSliderPagerAdapter = new ImageSliderPagerAdapter(ImageSlider.this, images);
         viewPager.setAdapter(imageSliderPagerAdapter);
         viewPager.setCurrentItem(images.length * ImageSliderPagerAdapter.LOOP_MULTIPLIER / 2);
+
+        eyeMoveDetector = cameraControlMainClass.attachObserverToEyeMoveDetector(this);
+        cameraControlMainClass.start();
+    }
+
+    public void nextImg() {
+        viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+    }
+
+    public void prevImg() {
+        viewPager.setCurrentItem(viewPager.getCurrentItem() - 1);
+    }
+
+    @Override
+    public void update() {
+        EyeMoveDetector.EYE_MOVE eyesMoved = eyeMoveDetector.getEyeMovedTo();
+        if (eyesMoved == EyeMoveDetector.EYE_MOVE.RIGHT) {
+            nextImg();
+        } else if (eyesMoved == EyeMoveDetector.EYE_MOVE.LEFT) {
+            prevImg();
+        }
     }
 }
