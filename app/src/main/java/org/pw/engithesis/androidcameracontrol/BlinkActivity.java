9d0b873e058331ee09d9a97521785f4c8c83f4e6
Maciej Adamski <madamskip1@gmail.com>
2021-12-08 11:56:26 +0100

    Refactor Blink activity: make blinkNotificationAdapter inline of BlinkActivity

--- app/src/main/java/org/pw/engithesis/androidcameracontrol/BlinkActivity.java
+++ app/src/main/java/org/pw/engithesis/androidcameracontrol/BlinkActivity.java
@@ -1,35 +1,66 @@
 package org.pw.engithesis.androidcameracontrol;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import org.pw.engithesis.androidcameracontrol.interfaces.Observer;
 
-public class BlinkActivity extends AppCompatActivity implements Observer {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
-    FaceAnalyser cameraControlMainClass;
-    EyeBlinkDetector blinkDetector;
-    private BlinkNotificationAdapter blinkNotificationAdapter;
+public class BlinkActivity extends AppCompatActivity implements Observer {
+    private int buttonsCounter = 0;
+    private LinearLayout blinkMsgBox;
+
+    @Override
+    public void update() {
+        addBox();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blink);
+        blinkMsgBox = findViewById(R.id.blink_msg_box);
 
-        blinkNotificationAdapter = new BlinkNotificationAdapter(this);
-        cameraControlMainClass = new FaceAnalyser(this);
-        blinkDetector = cameraControlMainClass.attachObserverToBlinkDetector(this);
-        cameraControlMainClass.start();
+        FaceAnalyser faceAnalyser = new FaceAnalyser(this);
+        faceAnalyser.attachObserverToBlinkDetector(this);
+        faceAnalyser.start();
     }
 
     private void addBox() {
-        blinkNotificationAdapter.addBox();
+        Button button = prepareButton();
+        blinkMsgBox.addView(button);
+        new Handler(Looper.getMainLooper()).postDelayed(() -> blinkMsgBox.removeView(button), 5000);
     }
 
+    private Button prepareButton() {
+        Button button = new Button(this);
+        String buttonText = "Mrugnięto o " + getTime() + ".";
+        button.setText(buttonText);
+        button.setBackgroundColor(ContextCompat.getColor(this, ((buttonsCounter % 2 == 0) ? android.R.color.holo_blue_light : android.R.color.holo_green_dark)));
+        buttonsCounter++;
 
-    @Override
-    public void update() {
-        addBox();
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        params.setMargins(10, 10, 10, 20);
+        button.setLayoutParams(params);
+
+        return button;
+    }
+
+    private String getTime() {
+        Date date = Calendar.getInstance().getTime();
+        DateFormat dateFormat = new SimpleDateFormat("'['HH:mm:ss:SSS']'", new Locale("pl", "PL"));
+
+        return dateFormat.format(date);
     }
 }
